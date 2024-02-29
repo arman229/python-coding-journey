1c3b969e98320a43b10155238ab589c6882c3073
Arman229 <armanashraf015@gmail.com>
2024-03-01 00:36:34 +0500

    practiced some topic of oop

---
+++ Python_Crash_Course/findallprimes/findallprimes/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='findallprimes',
+version='0.1.0',
+author='arman1804',
+author_email='armanashraf015@gmail.com',
+description='This package finds all prime numbers from 2 to that number ',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
